f8e8f872427901c9eb13e7970a454ff667466d05
PeterFromHell <peterdangw515jd@gmail.com>
2023-03-02 21:43:28 +0800

    add logout

--- client/components/SideBar.tsx
+++ client/components/SideBar.tsx
@@ -1,7 +1,11 @@
+"use client";
 import React from "react";
 import NewChat from "./NewChat";
+import { useSession, signOut } from "next-auth/react";
 
 const NavBar = () => {
+  const { data: session } = useSession();
+
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
@@ -13,6 +17,15 @@ const NavBar = () => {
           {/* map through the chart */}
         </div>
       </div>
+
+      {session && (
+        <img
+          src={session.user?.image!}
+          alt="Profile Pic"
+          className="h-12 w-12 rounded-full cursor-pointer mx-auto hover:opacity-50"
+          onClick={() => signOut()}
+        />
+      )}
     </div>
   );
 };
