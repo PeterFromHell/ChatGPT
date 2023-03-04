2eeb6e360760cc7befe5760bbdb1249fd6765e48
PeterFromHell <peterdangw515jd@gmail.com>
2023-03-04 11:19:47 +0800

    mark active chat

--- client/components/ChatRow.tsx
+++ client/components/ChatRow.tsx
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { ChatBubbleLeftIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { usePathname, useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { collection, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
 
 type Props = {
   id: string;
 };
 
 const ChatRow = ({ id }: Props) => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [active, setActive] = useState(false);
+
+  const [messages] = useCollection(
+    query(
+      collection(db, "users", session?.user?.email!, "chats", id, "messages"),
+      orderBy("createdAt", "asc")
+    )
+  );
+  useEffect(() => {
+    if (!pathname) return;
+
+    setActive(pathname.includes(id));
+  }, [pathname]);
+
   return (
-    <Link href={`/chat/${id}`} className={`chatRow justify-center`}>
+    <Link
+      href={`/chat/${id}`}
+      className={`chatRow justify-center ${active && "bg-gray-700/50"}`}
+    >
       <ChatBubbleLeftIcon className="w-5 h-5" />
-      <p className="flex-1 hidden md:inline-flex truncate">New Chat</p>
+      <p className="flex-1 hidden md:inline-flex truncate">
+        {messages?.docs[messages?.docs.length - 1]?.data().text || "New Chat"}
+      </p>
       <TrashIcon className="w-5 h-5 text-gray-700 hover:text-red-700" />
     </Link>
   );
