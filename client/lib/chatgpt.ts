b7d0bbea20c9cd5fd451795e08c88e30a987c695
PeterFromHell <peterdangw515jd@gmail.com>
2023-03-09 23:18:21 +0800

    get response from openai

--- client/lib/chatgpt.ts
+++ client/lib/chatgpt.ts
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apikey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
--- client/pages/api/askQuestion.ts
+++ client/pages/api/askQuestion.ts
@@ -1,11 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import admin from "firebase-admin";
+import { adminDb } from "../../firebaseAdmin";
 import query from "../../lib/queryApi";
 
 type Data = {
   answer: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
@@ -22,11 +24,25 @@ export default function handler(
   }
 
   // chatGPT query
-  const response = query(prompt, chatId, model);
+  const response = await query(prompt, chatId, model);
 
   const message: Message = {
     text: response || "ChatGPT was unable to find an answer for that!",
+    createdAt: admin.firestore.Timestamp.now(),
+    user: {
+      _id: "ChatGPT",
+      name: "ChatGPT",
+      avatar: "https://links.papareact.com/89k",
+    },
   };
 
-  res.status(200).json({ answer: "John Doe" });
+  await adminDb
+    .collection("users")
+    .doc(session?.user?.email)
+    .collection("chats")
+    .doc(chatId)
+    .collection("messages")
+    .add(message);
+
+  res.status(200).json({ answer: message.text });
 }
