220887ba3bfe1ebc41ed5ade0510d40439196c2e
PeterFromHell <peterdangw515jd@gmail.com>
2023-03-14 21:57:47 +0800

    start mobile app project

---
+++ mobile/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { View, Text, SafeAreaView } from "react-native";
+
+const App = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+export default App;
